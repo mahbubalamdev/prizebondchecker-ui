7e68c75969f7e379ee6039a885bb0b0416774969
Mahbub Alam <mahbub.alam@britecore.com>
2021-11-27 19:17:22 +0600

    Change redirect url

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,8 @@ export default {
           maxAge: 60 * 60 * 24 * 30
         },
         responseType: "token",
-        redirectUri: "https://prizebondchecker.netlify.app/login",
-        logoutRedirectUri: "https://prizebondchecker.netlify.app",
+        redirectUri: "https://www.abol-tabol.com/login",
+        logoutRedirectUri: "https://www.abol-tabol.com",
         clientId: "6meu16r4mnhc22sqr1diuknc4o",
         scope: ["email", "openid", "profile"],
         codeChallengeMethod: "S256"
