e2e67c3ac72876169925be2f31a436ae6a19019d
Mahbub Alam <mahbub.alam@britecore.com>
2021-11-27 19:00:22 +0600

    Use data table for showing user prizebonds

--- pages/prizebonds/me.vue
+++ pages/prizebonds/me.vue
@@ -19,28 +19,88 @@
                 </v-btn>
         </v-row>
         </v-card-text>
+        
        <v-card-text>
-        <v-list two-line flat>
-          <v-list-item-group>
-            <v-list-item v-for="(bond, i) in bonds" :key="i">
-              <v-list-item-action>
-                <v-checkbox readonly :input-value="bond.matches.length > 0"></v-checkbox>
-              </v-list-item-action>
+         <v-data-table
+          v-model="selected"
+          :headers="headers"
+          :items="bonds"
+          :items-per-page="5"
+          item-key="number"
+          :search="search"
+          show-select
+          flat
+        >
+        <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+                <v-btn fab color="brown" dark small>{{bonds.length | bengali_number}}</v-btn>
 
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="খুঁজে দেখুন"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="red"
+              dark
+              small
+              fab
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <v-row>
+                <v-col cols="12" align="center">আপনি কি প্রাইজবন্ড গুলো মুছে ফেলতে নিশ্চিত?</v-col>
+              </v-row>
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col v-for="(bond, i) in selected" :key="i">
+                  {{bond.number | bengali_number}}
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer/>
+              <v-btn @click="dialog=false">Cancel</v-btn>
+              <v-btn @click="removeBonds">OK</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+        </template>
+        <template v-slot:item.number="{item}">
+          {{item.number | bengali_number}}
+        </template>
+        <template v-slot:item.matches="{item}">
+          <v-list two-line flat>
+            <v-list-item>
               <v-list-item-content>
-                <v-list-item-title>{{ bond.number | bengali_number }}</v-list-item-title>
-                <v-list-item-subtitle v-for="(match, i) in bond.matches" :key="i">
+                <v-list-item-subtitle v-for="(match, i) in item.matches" :key="i">
                   ভাই জিতসেন! {{ match.prize }} {{ match.amount | bengali_number }} টাকা। ড্রঃ
                   {{ match.draw_number | bengali_number }} তম ({{ match.date | bengali_number }})</v-list-item-subtitle
                 >
-                <v-list-item-subtitle v-if="bond.matches.length == 0">পাওয়া যায় নি 😞</v-list-item-subtitle>
+                <v-list-item-subtitle v-if="item.matches.length == 0">পাওয়া যায় নি 😞</v-list-item-subtitle>
               </v-list-item-content>
-              <v-btn fab ripple x-small color="red" @click="removeBond(i)">
-                <v-icon class="white--text">mdi-close</v-icon>
-              </v-btn>
             </v-list-item>
-          </v-list-item-group>
-        </v-list>
+          </v-list>
+          </template>
+        </v-data-table>
+     
        </v-card-text>
       </v-card>
       
@@ -60,6 +120,13 @@ export default {
     return {
       newBond: "",
       bonds: [],
+      selected: [],
+      dialog: false,
+      search: "",
+      headers: [
+        { text: 'প্রাইজবন্ড', value: 'number' },
+        { text: 'ফলাফল', value: 'matches' },
+      ]
     };
   },
 
@@ -71,7 +138,9 @@ export default {
           matches: search_prizebond(bond["BondNumber"]),
         };
       });
+      
     });
+    this.bonds.sort((a,b)=>a.number>b.number);
   },
   created() {},
   methods: {
@@ -81,17 +150,33 @@ export default {
         return;
       }
       value = conver_to_english(value)
-      this.bonds.push({
-        number: value,
-        matches: search_prizebond(value),
-      });
-      this.newBond = "";
-      this.$axios.post("/prizebonds/", { number: value });
+      
+      
+      this.$axios.post("/prizebonds/", { number: value }).then(response=>{
+        this.bonds.push({
+          number: value,
+          matches: search_prizebond(value),
+        });
+        this.newBond = "";
+      })
     },
-    removeBond(index) {
-      let value = this.bonds[index]["number"];
-      this.$axios.delete("/prizebonds/", { data: { number: value } });
-      this.bonds.splice(index, 1);
+    removeBonds() {
+      console.log(this.selected)
+      if (this.selected.length>0){
+        let bond_numbers = this.selected.map(item=>item.number)
+        this.$axios.delete("/prizebonds/", { data: {bonds: bond_numbers} }).then((response)=>{
+
+          for (let removed of this.selected){
+            let index = this.bonds.indexOf(removed)
+            this.bonds.splice(index, 1)
+          }
+          this.selected = []
+          this.dialog = false
+        })
+      }else{
+        this.dialog = false
+      }
+      
     },
   },
   computed: {
